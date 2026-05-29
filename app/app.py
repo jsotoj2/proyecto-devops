0fe9ed37fa3224800cc274441be41ea4c6f5251c
Jose <jsotoj2@miumg.edu.gt>
2026-05-28 23:49:18 -0600

    Primer commit

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return """
+    <h1>Proyecto DevOps</h1>
+    <h2>Sistemas Operativos II</h2>
+    <p>Docker + AWS + CI/CD funcionando</p>
+    """
+
+app.run(host='0.0.0.0', port=5000)
