d0b4ae301889785f96a6e27e5fd9e5e56262646c
Jose <jsotoj2@miumg.edu.gt>
2026-05-29 18:26:20 -0600

    version 2

--- app/app.py
+++ app/app.py
@@ -2,14 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
-<h1>CI/CD funcionando correctamente</h1>
+
 
 @app.route('/')
 def home():
     return """
     <h1>Proyecto DevOps</h1>
     <h2>Sistemas Operativos II</h2>
-    <p>Docker + AWS + CI/CD funcionando</p>
+    <p>Docker + CI/CD funcionando</p>
+    <h1>CI/CD funcionando correctamente</h1>
     """
 
 app.run(host='0.0.0.0', port=5000)
